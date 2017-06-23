6e7d6bc90851882ce46eee899057324d07e88ff3
Tamim Ibn Aman <tamimibnaman@yahoo.com>
2017-06-23 16:42:53 +0600

    GameScene and ViewController ready, sounds and images added

--- Swiftris/GameViewController.swift
+++ Swiftris/GameViewController.swift
@@ -11,31 +11,23 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    var scene: GameScene!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        let skView = view as! SKView
+        skView.isMultipleTouchEnabled = true
+        
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
+        
     }
-
+    
     override var shouldAutorotate: Bool {
         return true
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
             return .allButUpsideDown
@@ -43,12 +35,12 @@ class GameViewController: UIViewController {
             return .all
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
